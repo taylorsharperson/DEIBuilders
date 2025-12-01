7daf7258ce73d1bccdf9da8ef5a9f79b496bf247
Taylor Sharperson <133130597+taylorsharperson@users.noreply.github.com>
2025-12-01 18:13:22 -0500

    Update Dashboard.jsx

--- deibuilders/src/pages/Dashboard.jsx
+++ deibuilders/src/pages/Dashboard.jsx
@@ -1,1 +1,11 @@
 import MentorRequest from '../components/MentorRequest';
+function Dashboard() {
+  return (
+    <div>
+      <h1>Dashboard</h1>
+      <MentorRequest />
+    </div>
+  );
+}
+
+export default Dashboard;
